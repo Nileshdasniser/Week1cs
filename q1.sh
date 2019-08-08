64cb563ef883851887dce53a4a99797522fecac3
Nileshdasniser <53328627+Nileshdasniser@users.noreply.github.com>
2019-08-08 21:34:46 +0530

    Add files via upload
    Assignment Completed

---
+++ q1.sh
@@ -0,0 +1,5 @@
+echo "Hello, Madame/Monsieur Please enter your name !"
+read name
+mkdir $name
+touch $name/$name.cpp
+echo "Respected idiot your folder has been created"
---
+++ q2.sh
@@ -0,0 +1,11 @@
+echo "Hello, please enter your precious name again "
+read name
+mkdir $name
+touch $name/"name$"1.cpp
+touch $name/"name$"2.cpp
+touch $name/"name$"3.cpp
+touch $name/"name$"4.cpp
+touch $name/"name$"5.cpp
+echo "Respected Sir/madam, Your Folder has been created"
+echo "Your file has been created"
+
